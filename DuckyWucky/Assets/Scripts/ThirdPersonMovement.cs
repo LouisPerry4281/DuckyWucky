f2dfb389d030439d137bc67a9fb227546bd3d485
Tom Prince <123157224+TPrincy@users.noreply.github.com>
2023-09-08 15:28:50 +0100

    Small Ducky changes
    finished up camera system and basic movement

--- DuckyWucky/Assets/Scripts/ThirdPersonMovement.cs
+++ DuckyWucky/Assets/Scripts/ThirdPersonMovement.cs
@@ -6,8 +6,12 @@ public class ThirdPersonMovement : MonoBehaviour
 {
 
     public CharacterController controller;
+    public Transform cam;
 
     public float speed = 6f;
+    public float turnSmothTime = 0.1f;
+    float turnSmoothVelocity;
+
 
     void Update()
     {
@@ -17,7 +21,14 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            controller.Move(direction * speed * Time.deltaTime);
-        }
+            //Atan2 turns the player to face the diraction was have inpted with the movement keys
+            //Rad2Deg converts the units into degrees
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }    
     }
 }
