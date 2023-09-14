96bff4104d136d93ce893227ed98720a302a1335
Tom Prince <123157224+TPrincy@users.noreply.github.com>
2023-09-14 18:03:46 +0100

    DUCKYYYYY

---
+++ DuckyWucky/Assets/Scripts/WeaponCollisionScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponCollisionScript : MonoBehaviour
+{
+
+    public float AttackSpeed = 4f;
+
+    Collider WeaponHitbox;
+
+    void Start()
+    {
+        WeaponHitbox = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && WeaponHitbox == false)
+        {
+            WeaponHitbox.enabled = true;
+            print("true");
+            Invoke("AttackTime", AttackSpeed);
+        }
+    }
+
+    public void AttackTime()
+    {
+        WeaponHitbox.enabled = false;
+        print("false");
+    }
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            print("hit");
+            
+        }
+    }
+}
